eb4a67113525b594d6589425366332385eea4201
Liam J Gahan <liamgahan09@gmail.com>
2025-04-04 16:53:39 +0100

    Added connection to the Aiven database and tested connection by displaying the schema to index
    .env file and .gitignore was used to hide connection details. .gitattributes was altered to stop "This diff contains a change in line endings from 'LF' to 'CRLF'".

--- api.py
+++ api.py
@@ -1,14 +1,43 @@
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
+import mysql.connector
+from dotenv import load_dotenv
+import os
 
 # Configure application
 app = Flask(__name__)
 
-@app.route("/")
+# Database config (Aiven)
+def create_connection():
+    connection = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME'),
+        ssl_ca=os.getenv('SSL_CA_PATH')
+    )
+    return connection 
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+
+    connection = create_connection()
+    cursor = connection.cursor()
+    
+    cursor.execute("""
+        SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = 'defaultdb';
+    """)
+    
+    schema_data = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return render_template('index.html', schema_data=schema_data)
 
 # Remove for deployment
 if __name__ == '__main__':
