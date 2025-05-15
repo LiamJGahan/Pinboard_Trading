3b6cf2edd928e9a096b3b1ac07be256332dcd51f
LiamJGahan <liamgahan09@gmail.com>
2025-05-15 22:23:52 +0100

    Commented out local port code for deployment

--- api.py
+++ api.py
@@ -554,6 +554,6 @@ def privacy():
     return render_template("privacy.html")
 
 
-# Remove for deployment
-if __name__ == '__main__':
-    app.run(port=5002)
+# # Remove for deployment
+# if __name__ == '__main__':
+#     app.run(port=5002)
